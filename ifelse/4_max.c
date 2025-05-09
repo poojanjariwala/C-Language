ebee0470d3c84dc4b75a6d332ee98aca676b92ed
Poojan Jariwala <poojanjari@gmail.com>
2025-05-09 11:10:11 +0530

    Add files via upload

---
+++ ifelse/4_max.c
@@ -0,0 +1,119 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+
+
+int a,b,c,d;
+
+printf("Enter a: ");
+scanf("%d",&a);
+
+printf("Enter b: ");
+scanf("%d",&b);
+
+printf("Enter c: ");
+scanf("%d",&c);
+
+printf("Enter d: ");
+scanf("%d",&d);
+
+if(a==b && b==c && c==d)
+{
+	printf("a,b,c and d all are equal..");
+}
+
+else
+	{
+		if(b>a)
+		{
+			if(b>c)
+			{
+				if(b>d)
+				{
+					printf("b is MAX");	
+				}
+				else
+				{
+					printf("d is MAX");
+				}
+				
+			}
+			else
+			{
+				if(c>a)
+				{
+					if(c>d)
+					{
+						printf("c is MAX");	
+					}
+					else
+					{
+						printf("d is MAX");
+					}
+				}
+				else
+				{
+					if(a>d)
+					{
+						if(a>b)
+						{
+							printf("a is MAX");
+						}
+						else
+						{
+							printf("b is MAX");
+						}
+					}
+					else
+					{
+						if(d>b)
+						{
+							if(d>c)
+							{
+								printf("d is MAX");	
+							}
+						else
+						{
+							printf("c is MAX");
+						}
+							
+						}
+					}
+				}
+				
+			}
+		}
+		
+		else
+		{
+			if(a>c)
+			{
+				if(a>d)	
+				{
+					printf("a is MAX");
+				}
+				else
+				{
+					printf("d is MAX");
+				}
+			}
+			else
+			{
+				if(c>b)
+				{
+					if(c>d)
+					{
+						printf("c is MAX");	
+					}
+					else
+					{
+						printf("d is MAX");
+					}
+				}	
+			}
+		}
+	}
+}
+	
