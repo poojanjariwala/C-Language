6d926d6cf2cf6c9d8fd5f34eca9b034a86b3e3f2
Poojan Jariwala <poojanjari@gmail.com>
2025-05-16 10:19:58 +0530

    Create Basic_info.c

---
+++ Basic/Basic_info.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	printf("Name      : Poojan Jariwala\n");
+	printf("Mobile no : 8200344429\n");
+	printf("Address   : Satyanagar,Udhana,Surat\n");
+	printf("Email     : poojanjariwala1@gmail.com\n");
+	
+}
