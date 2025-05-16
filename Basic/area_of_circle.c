bcb03e5b800b1721c3e9a5340477d5a679104d5e
Poojan Jariwala <poojanjari@gmail.com>
2025-05-16 16:43:43 +0530

    Add files via upload

---
+++ Basic/area_of_circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int r=5;
+	float a=3.14*r*r;
+	
+	printf("%.1f",a);
+
+}
---
+++ Basic/arithmetic_operator.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int x=5,y=3;
+	int z=x+y;
+	int a=x-y;
+	int b=x*y;
+	int c=x/y;
+	int d=x%y;
+	
+	printf("%d\n",z);
+	printf("%d\n",a);
+	printf("%d\n",b);
+	printf("%d\n",c);
+	printf("%d\n",d);
+}
