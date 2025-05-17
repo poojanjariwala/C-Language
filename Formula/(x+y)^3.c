858b073cb9164a9df93739619ca41eaac84487a3
Poojan Jariwala <poojanjari@gmail.com>
2025-05-17 08:35:04 +0530

    Add files via upload

---
+++ Formula/(x+y)^3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int x,y,a;
+	printf("Enter x: ");
+	scanf("%d",&x);
+	printf("Enter y: ");
+	scanf("%d",&y);
+	
+	
+	a=(x+y)*(x+y)*(x+y);
+	
+	printf("%d",a);
+
+}
---
+++ Formula/(x+y+z)^2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int x,y,z,a;
+	printf("Enter x: ");
+	scanf("%d",&x);
+	printf("Enter y: ");
+	scanf("%d",&y);
+	printf("Enter z: ");
+	scanf("%d",&z);
+	
+	
+	a=(x+y+z)*(x+y+z);
+	
+	printf("%d",a);
+
+}
