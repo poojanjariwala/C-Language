8755c928a12b76e0d702c7c5a7dc575e74163eee
Poojan Jariwala <poojanjari@gmail.com>
2025-05-17 10:22:11 +0530

    Add files via upload

---
+++ Greenhorns/pattern.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main() 
+{
+	
+	printf("\t *\t*\n");
+    printf("    *      *\n");
+    printf("*      *\n");
+    printf("    *      *\n");
+    printf("\t *\t*\n\n\n");
+
+}
+
---
+++ Greenhorns/pattern_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main() 
+{
+    printf("    *   *   *   *   *   *   *\n\n");
+    printf("*   *   *   *   *   *   *   *\n");
+    printf("*       *                   *\n");
+    printf("*       *       *   *   *   *\n");
+    printf("*       *       *       *   *\n");
+    printf("*   *   *   *   *   *   *   *\n");
+
+}
+
